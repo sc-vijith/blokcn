add0c0eaf26bd78e28b163a05cd3d99556509c4f
sc-vijith <v7813985@gmail.com>
2025-08-28 13:37:36 +0530

    seperator component add to demo (#83)
    Co-authored-by: vijithan <virv@sitecore.net>

--- src/app/demo/[name]/ui/separator.tsx
+++ src/app/demo/[name]/ui/separator.tsx
@@ -5,19 +5,25 @@ export const separator = {
   components: {
     Default: (
       <div>
-        <div className="space-y-1">
-          <h4 className="font-medium text-sm leading-none">Radix Primitives</h4>
-          <p className="text-muted-foreground text-sm">
-            An open-source UI component library.
-          </p>
-        </div>
-        <Separator className="my-4" />
-        <div className="flex h-5 items-center space-x-4 text-sm">
-          <div>Blog</div>
-          <Separator orientation="vertical" />
-          <div>Docs</div>
-          <Separator orientation="vertical" />
-          <div>Source</div>
+        <div className="flex rounded-t-md bg-white ">
+          <div className="">
+            <div className="flex flex-col gap-1">
+              <div className="text-sm leading-none font-medium">
+                Tailwind CSS
+              </div>
+              <div className="text-muted-foreground text-sm">
+                A utility-first CSS framework.
+              </div>
+            </div>
+            <Separator className="my-4" />
+            <div className="flex h-5 items-center gap-4 text-sm">
+              <div>Blog</div>
+              <Separator orientation="vertical" />
+              <div>Docs</div>
+              <Separator orientation="vertical" />
+              <div>Source</div>
+            </div>
+          </div>
         </div>
       </div>
     ),
