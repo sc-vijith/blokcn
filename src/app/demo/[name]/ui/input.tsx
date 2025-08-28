4762c2cb9cdb73e28a18f22c281ad7754b273be8
Ishanka Lakshan <sc.ishanka@gmail.com>
2025-08-28 15:33:30 +0530

    Integration of the input component to the doc site (#87)
    * Integration of the input component to the doc site
    
    * Integration of the input component to the doc site revert
    
    * Integration of the input component to the doc site

--- src/app/demo/[name]/ui/input.tsx
+++ src/app/demo/[name]/ui/input.tsx
@@ -1,29 +1,31 @@
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 export const input = {
   name: "input",
   components: {
-    Default: <Input placeholder="Default" />,
-    Email: <Input type="email" placeholder="Email" />,
-    WithLabel: (
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="email">Label</Label>
-        <Input type="email" id="email" placeholder="With label" />
+    Email: (
+      <div className="w-75 ml-2">
+        <Input type="email" placeholder="Email" />
       </div>
     ),
-    WithButton: (
-      <div className="flex w-full max-w-sm items-center space-x-2">
-        <Input type="email" placeholder="With button" />
-        <Button type="submit">Button</Button>
+    Text: (
+      <div className="w-75 ml-2">
+        <Input type="text" placeholder="Text" aria-invalid="true" />
       </div>
     ),
-
+    Password: (
+      <div className="w-75 ml-2">
+        <Input type="password" placeholder="Password" />
+      </div>
+    ),
     File: (
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="picture">File</Label>
-        <Input id="picture" type="file" />
+      <div className="w-75 ml-2">
+        <Input type="file" placeholder="File" />
+      </div>
+    ),
+    Disabled: (
+      <div className="w-75 ml-2">
+        <Input disabled placeholder="Disabled" />
       </div>
     ),
   },
